d6c932784bb523ad959623138f73c953ed71d5e3
Md Alamin <alaminduc516@gmail.com>
2022-11-01 22:44:17 +0600

    Update server link

--- src/Pages/Courses/Courses/Courses.js
+++ src/Pages/Courses/Courses/Courses.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { ListGroup } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
 import Course from '../../Course/Course/Course';
 
 const Courses = () => {
     const courses = useLoaderData();
 
     return (
-        <div className='courses'>
-            {
-                courses.map(course => <Course
-                    key={course.id}
-                    course={course}
-                ></Course>)
-            }
-        </div>
-)
+        <>
+            <div className='d-lg-none d-block'>
+                {
+                    courses.map(course => <Link key={course.id} className='text-decoration-none' to={`/course/${course.id}`}><ListGroup>
+                        <ListGroup.Item className='mb-2'>{course.title}</ListGroup.Item>
+                    </ListGroup></Link>)
+                }
+            </div>
+            <div className='courses'>
+                {
+                    courses.map(course => <Course
+                        key={course.id}
+                        course={course}
+                    ></Course>)
+                }
+            </div>
+        </>
+    )
 };
 
 export default Courses;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,12 +5,12 @@ const Home = () => {
     <div className='banner'>
       <Container>
         <Row>
-          <Col lg={12} className='text-center'>
+          <div className='text-center'>
             <div className='banner-content text-light'>
               <h2>Tech Learning Mate</h2>
               <p>Most apps need to know the identity of a user. Knowing a user's identity allows an app to securely save user data in the cloud and provide the same personalized experience across all of the user's devices. </p>
             </div>
-          </Col>
+          </div>
         </Row>
       </Container>
     </div>
